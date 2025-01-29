517903e5d90a0223eafa345982e7fe179f4fe43a
meryemidboucair <meryemidboucair22@gmail.com>
2025-01-29 11:13:05 +0100

    remove updated Model

--- aitunes/autoencoders/autoencoders_modules.py
+++ aitunes/autoencoders/autoencoders_modules.py
@@ -83,54 +83,11 @@ class VariationalAutoEncoder(nn.Module):
         return z, self._decoder(z), mu, log_var
     
 
-class VAE_Model(nn.Module):
-    def __init__(self, input_dim, latent_dim):
-        super(VAE_Model, self).__init__()
-        
-        # Encodeur
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, 256),  # Couche d'entrée
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64)
-        )
-        
-        # Paramètres latents
-        self.mu = nn.Linear(64, latent_dim)      # Moyenne de la distribution latente
-        self.logvar = nn.Linear(64, latent_dim)  # Logarithme de la variance
-
-        # Décodeur
-        self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 64),  # Couche d'entrée pour décodeur
-            nn.ReLU(),
-            nn.Linear(64, 128),
-            nn.ReLU(),
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.Linear(256, input_dim),   # Couche de sortie pour reconstruire l'entrée
-            nn.Sigmoid()                 # Normalisaer 
-        )
-
-    def reparameterization(self, mu, log_var):
-        std = torch.exp(0.5 * log_var)  
-        eps = torch.randn_like(std)      
-        return mu + eps * std     
-     
-     
-    def forward(self, x):
-        x = self.encoder(x)              
-        mu = self.mu(x)                  
-        log_var = self.logvar(x)          
-        z = self.reparameterization(mu, log_var)  
-        return z,self.decoder(z), mu, log_var  
-    
-
-class VAE(nn.Module):
+class CVAE(nn.Module):
 
     def __init__(self, input_shape, conv_filters, conv_kernels, conv_strides, latent_space_dim):
 
-        super(VAE, self).__init__()
+        super(CVAE, self).__init__()
         self.input_shape = input_shape  
         self.conv_filters = conv_filters #le nombre de filtres dans chaque couche de convolution
         self.conv_kernels = conv_kernels
