Fixed CVAE by disabling the encoder Batch Normalization and moving it in the decoder.

aitunes/modules/autoencoder_modules.py
@@ -118,8 +118,8 @@ class CVAE(nn.Module):
         # Couches convolutionnelles
         for out_channels, kernel_size, stride in zip(self.conv_filters, self.conv_kernels, self.conv_strides):
             layers.append(nn.Conv2d(input_channels, out_channels, kernel_size, stride, padding=kernel_size//2))
+            # layers.append(nn.BatchNorm2d(out_channels)) #Normalisation. Removed from the encoder because batch-wide normalization doesn't fit well with KL divergence
             layers.append(nn.ReLU())  #non-linéarité
-            layers.append(nn.BatchNorm2d(out_channels)) #Normalisation
             input_channels = out_channels
         
         layers.append(nn.Flatten())
@@ -162,15 +162,15 @@ class CVAE(nn.Module):
             padding, (p_h, p_w) = self._compute_decoder_padding(i)
             
             layers.append(nn.ConvTranspose2d(input_channels, out_channels, kernel_size, stride, padding=padding, output_padding=(p_h, p_w)))
-            layers.append(nn.ReLU())
             layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU())
             
             input_channels = out_channels
 
         padding, (p_h, p_w) = self._compute_decoder_padding(0)
         layers.append(nn.ConvTranspose2d(input_channels, self.input_shape[0], self.conv_kernels[0], self.conv_strides[0], padding=padding, output_padding=(p_h, p_w)))
         layers.append(nn.Sigmoid())
-        # layers.append(nn.ReLU())
+        
         return nn.Sequential(*layers)
     
     def _calculate_shape_before_bottleneck(self):